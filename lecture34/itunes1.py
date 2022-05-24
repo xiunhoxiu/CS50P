7ea115cc61fff5ecf7843d851711f23ef1ae5dfe
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-24 14:23:17 +0200

    API request

---
+++ lecture34/itunes1.py
@@ -0,0 +1,14 @@
+import json
+import sys
+import requests
+
+if len(sys.argv) != 2:
+    sys.exit()
+
+# API request - changed limit from 1 to 50 instead
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
+print(json.dumps(response.json(), indent=2))
+
+o = response.json()
+for result in o["results"]:
+    print(result["trackName"])
