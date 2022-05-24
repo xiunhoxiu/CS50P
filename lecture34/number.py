b0cae0e7d48312fdc9e73ef281ec6ae176cd6155
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-05-24 12:44:20 +0200

    number.py exceptions

---
+++ lecture34/number.py
@@ -0,0 +1,13 @@
+def main():
+    x = get_int("What's x?")
+    print(f"you've typed x is {x}")
+
+def get_int(prompt):
+    while(true)
+        try:
+            return int(input(prompt))
+        except ValueError:
+            pass
+
+
+main()
