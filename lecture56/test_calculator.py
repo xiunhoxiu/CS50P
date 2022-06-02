1f82cfa1045640e4015268c1907742af2df64532
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-06-02 12:35:20 +0200

    using pytest

--- lecture56/test_calculator.py
+++ lecture56/test_calculator.py
@@ -21,7 +21,11 @@ def test_square():
         assert square(-3) == 9
     except AssertionError:
         print("-3 squared was not 9")
-
+    try:
+        assert square(0) == 0
+    except AssertionError:
+        print("0 squared was not 0")
+ª
 
 if __name__ == "__main__":
     main()
