f31f56ec5bd3e85e70db35398b66e1f141a46c4d
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-06-02 13:14:00 +0200

    test in categories with pytest

--- lecture56/test_calculator2.py
+++ lecture56/test_calculator2.py
@@ -1,10 +1,14 @@
 from calculator import square
 
-# using pytest
-def test_square():
+def test_positive():
     assert square(2) == 4
     assert square(3) == 9
+
+def test_negative():
     assert square(-2) == 4
     assert square(-3) == 9
+
+def test_zero():
     assert square(0) == 0
-    
+
+
