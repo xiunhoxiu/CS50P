63f85e69a4290e93a9993f6868519aa5f9de2873
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-06-09 16:08:10 +0200

    writing csv

--- lecture56/students3.py
+++ lecture56/students3.py
@@ -3,9 +3,9 @@ import csv
 students = []
 
 with open("students1.csv") as file:
-    reader = csv.reader(file)
-    for name, home in reader:
-        students.append({"name": name, "home": home})
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name": row["name"], "home": row["home"], "house": row["house"]})
 
 for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is in {student['home']}")
+    print(f"{student['name']} is in {student['house']} from {student['home']}")
