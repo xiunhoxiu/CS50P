7ce891445fa7ca6ab19520f3276047c3055695d2
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-06-09 15:48:15 +0200

    csv module and .reader

--- lecture56/students2.py
+++ lecture56/students2.py
@@ -3,15 +3,8 @@ students = []
 with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
-        # defining keys and values instead of leaving dictionary empty
         student = {"name": name, "house": house}
         students.append(student)
 
-def get_name(student):
-    return student["name"]
-
-def get_house(student):
-    return student["house"]
-
-for student in sorted(students, key=get_house, reverse=True):
+for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is in {student['house']}")
---
+++ lecture56/students3.py
@@ -0,0 +1,11 @@
+import csv
+
+students = []
+
+with open("students1.csv") as file:
+    reader = csv.reader(file)
+    for name, home in reader:
+        students.append({"name": name, "home": home})
+
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is in {student['home']}")
