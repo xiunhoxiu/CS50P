9728af5d5090a4c97a2e4233f744aa90e7600b16
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-06-14 14:07:26 +0200

    modified

--- lecture7/validate.py
+++ lecture7/validate.py
@@ -2,7 +2,7 @@ import re
 
 email = input("What's your email? ").strip()
 
-if re.search("@", email):
+if re.search("^\w+@\w+\.(com|edu|gov|net|dk|org)$", email):
     print("Valid")
 else:
     print("Invalid")
