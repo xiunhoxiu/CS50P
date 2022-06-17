3a2a8a5c513f95a914bb19873f37b9618c05c660
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-06-17 23:49:44 +0200

    walrus operator added

--- lecture7/twitter.py
+++ lecture7/twitter.py
@@ -1,4 +1,6 @@
+import re
+
 url = input("URL: ").strip()
 
-username = url.removeprefix("https://twitter.com/")
-print(f"Username: {username}")
+if matches := re.search("^https?://(www\.)?twitter\.com/(.+)$", url, re.IGNORECASE):
+    print(f"Username:", matches.group(2))
