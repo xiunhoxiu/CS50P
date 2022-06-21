53972bb87636201f5175bf390a9e4e4a6f637fc2
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-06-21 15:10:55 +0200

    global keyword

--- lecture9/bank.py
+++ lecture9/bank.py
@@ -1,3 +1,5 @@
+balance = 0
+
 def main():
     balance = 0
     print("Balance:", balance)
@@ -6,10 +8,12 @@ def main():
     print("New Balance:", balance)
 
 def deposit(n):
+    global balance
     balance += n
 
 
 def withdarw(n):
+    global balance
     balance -= n
 
 if __name__ == "__main__":
