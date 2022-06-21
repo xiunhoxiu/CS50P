84240a8921a8b05848cfbc86144baf3afb9b515b
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-06-21 15:12:21 +0200

    modified

--- lecture9/bank.py
+++ lecture9/bank.py
@@ -1,7 +1,6 @@
 balance = 0
 
 def main():
-    balance = 0
     print("Balance:", balance)
     deposit(100)
     withdraw(50)
@@ -12,7 +11,7 @@ def deposit(n):
     balance += n
 
 
-def withdarw(n):
+def withdraw(n):
     global balance
     balance -= n
 
