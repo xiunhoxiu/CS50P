430cb9f08464bd950d75124b5834e06fe031cd8e
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-06-22 10:47:31 +0200

    type hints and mypy

--- lecture9/meows.py
+++ lecture9/meows.py
@@ -1,6 +1,6 @@
-def meow(n: int):
-    for _ in range(n):
-        print("meow")
+def meow(n: int) -> str:
+    return "meow\n" * n
 
 number: int = int(input("Number: "))
-meow(number)
+meows: str = meow(number)
+print(meows, end="")
