ed8adeb93bd71f170dbe03a855d1bc4e0a009037
Thien Ly Buu Huynh <45794429+xiunhoxiu@users.noreply.github.com>
2022-06-22 14:47:03 +0200

    list comprehensions with conditionals

---
+++ lecture9/gryffindors1.py
@@ -0,0 +1,9 @@
+students = [
+   {"name": "Hermione", "house": "Gryffindor"},
+   {"name": "Harry", "house": "Gryffindor"},
+   {"name": "Ron", "house": "Gryffindor"},
+   {"name": "Draco", "house": "Slyterin"},
+   {"name": "Padma", "house": "Ravenclaw"} 
+]
+
+def is_
